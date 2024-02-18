b43122e738169fc4208d891f2636ea7c20dcfd68
karl ducta <karlducta022@gmail.com>
2024-02-18 14:44:16 +0800

    Create Link to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Homepage from "./pages/Homepage"
-import Products from "./pages/Products"
+import Product from "./pages/Product"
 import Pricing from "./pages/Pricing"
 import PageNotFound from "./pages/PageNotFound"
 
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="products" element={<Products />} />
+        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,12 @@
 import PageNav from "../components/PageNav"
 
-function Homepage() {
+function Product() {
     return (
         <div>
             <PageNav />
-            <h1>WorldWise</h1>
+            <h1>Product</h1>
         </div>
     )
 }
 
-export default Homepage
+export default Product
