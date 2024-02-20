ba415ebda134249b41839bb548d43f0e7dcede88
karl ducta <karlducta022@gmail.com>
2024-02-20 16:58:33 +0800

    Creating CitiesContext ---- Context API

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,45 @@
+const { createContext, useState, useEffect } = require("react");
+
+const CitiesContext = createContext();
+
+const BASE_URL = "http://localhost:8000";
+
+const flagemojiToPNG = (flag) => {
+  var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join("");
+  return (
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+};
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        alert("There was an error loading the data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{ cities, isLoading, flagEmoji: flagemojiToPNG }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };
