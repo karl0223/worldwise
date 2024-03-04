83f0b0b2de45609f417f4a59026bdbedb4d5491a
karl ducta <karlducta022@gmail.com>
2024-03-05 02:09:11 +0800

    changed the base URL of the cities API

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -8,7 +8,7 @@ import {
 
 const CitiesContext = createContext();
 
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://melted-trusting-apparel.glitch.me/";
 
 const intialState = {
   cities: [],
